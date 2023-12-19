54c5d6ce97bd92fb0651d95d74fa7b475180394b
fqrums <wotbusb@gmail.com>
2023-12-20 00:24:18 +0100

    delete useless shit + update web ig

--- snow.js
+++ snow.js
@@ -39,10 +39,10 @@ var snowStorm = (function(window, document) {
     this.freezeOnBlur = false;       // Only snow when the window is in focus (foreground.) Saves CPU.
     this.flakeLeftOffset = 0;       // Left margin/gutter space on edge of container (eg. browser window.) Bump up these values if seeing horizontal scrollbars.
     this.flakeRightOffset = 0;      // Right margin/gutter space on edge of container
-    this.flakeWidth = 8;            // Max pixel width reserved for snow element
-    this.flakeHeight = 8;           // Max pixel height reserved for snow element
-    this.vMaxX = 5;                 // Maximum X velocity range for snow
-    this.vMaxY = 4;                 // Maximum Y velocity range for snow
+    this.flakeWidth = 4;            // Max pixel width reserved for snow element
+    this.flakeHeight = 4;           // Max pixel height reserved for snow element
+    this.vMaxX = 2;                 // Maximum X velocity range for snow
+    this.vMaxY = 2;                 // Maximum Y velocity range for snow
     this.zIndex = 0;                // CSS stacking order applied to each snowflake
   
     // --- "No user-serviceable parts inside" past this point, yadda yadda ---
